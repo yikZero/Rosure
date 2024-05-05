4b5117ae4fc08b616380d21f1764e82fda62bee6
yikZero <i@yikzero.com>
2024-05-05 22:46:54 +0800

    feat: added no result tips

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,17 @@ export default function Home({
 
   return (
     <>
-      <div className="grid grid-cols-5 gap-4">
-        {filteredLinks.map((link, index) => (
-          <LinkCard key={index} link={link} />
-        ))}
-      </div>
+      {filteredLinks.length > 0 ? (
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-5">
+          {filteredLinks.map((link, index) => (
+            <LinkCard key={index} link={link} />
+          ))}
+        </div>
+      ) : (
+        <div className="mt-12 flex justify-center">
+          <div className="select-none bg-gradient-to-b from-primary to-primary/50 bg-clip-text text-2xl font-medium text-transparent">{`NO RESULT ABOUT "${query}"`}</div>
+        </div>
+      )}
     </>
   );
 }
