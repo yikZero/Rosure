5f23f4b521adb6fca17b2e59ec5ba0e845104a84
yikZero <i@yikzero.com>
2024-05-05 22:49:07 +0800

    perf: adjust mobile layout

--- app/list/[slug]/page.tsx
+++ app/list/[slug]/page.tsx
@@ -32,7 +32,7 @@ export default function List({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-5">
         {categoryLinks.map((link, index) => (
           <LinkCard key={index} link={link} />
         ))}
