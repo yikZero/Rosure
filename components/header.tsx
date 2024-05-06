6cd4fc7fd0af024eb55a29ae1acc9d251d33d551
yikZero <i@yikzero.com>
2024-05-06 20:04:32 +0800

    perf: adjust search width on mobile

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ import { Suspense } from 'react';
 export default function Header() {
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-50 flex h-16 flex-row items-center justify-between border-b border-border bg-body-background px-4 md:gap-6 md:px-5">
+      <header className="sticky inset-x-0 top-0 z-50 flex h-16 flex-row items-center justify-between gap-8 border-b border-border bg-body-background px-4 md:px-5">
         <Link href="/" className="group flex flex-row items-center gap-2">
           <Logo className="size-8 rounded-md group-hover:opacity-80" />
           <span className="hidden text-sm font-medium text-primary sm:block">
