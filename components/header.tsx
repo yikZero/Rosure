99f237b9853924b812c70ab179148ce7f8ba2c8e
yikZero <i@yikzero.com>
2024-05-06 18:09:11 +0800

    perf: adjust mobile search position

--- components/header.tsx
+++ components/header.tsx
@@ -8,14 +8,14 @@ import { Suspense } from 'react';
 export default function Header() {
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-50 flex h-16 flex-row items-center justify-between gap-3 border-b border-border bg-body-background px-4 md:px-5">
+      <header className="sticky inset-x-0 top-0 z-50 flex h-16 flex-row items-center justify-between border-b border-border bg-body-background px-4 md:gap-6 md:px-5">
         <Link href="/" className="group flex flex-row items-center gap-2">
           <Logo className="size-8 rounded-md group-hover:opacity-80" />
           <span className="hidden text-sm font-medium text-primary sm:block">
             Rosure
           </span>
         </Link>
-        <div className="block sm:hidden">
+        <div className="-mr-2 block sm:hidden">
           <Suspense>
             <Search placeholder="Search Something" />
           </Suspense>
