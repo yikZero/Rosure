7cf8fd11b34df6c3b01bc062ab2c0f37dff87c40
yikZero <i@yikzero.com>
2024-07-31 14:34:31 +0800

    feat: add UI inspired Source

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,17 +4,31 @@ export default function Footer() {
   return (
     <>
       <footer className="flex flex-col items-center justify-center gap-1 text-nowrap border-t border-border px-4 pb-6 pt-8 text-secondary sm:flex-row">
-        <div>
-          Built by
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline pl-1 font-medium text-primary underline underline-offset-4"
-            href="https://ro.yikzero.com/"
-          >
-            yikZero
-          </Link>
-          .
+        <div className="flex flex-row gap-1">
+          <div>
+            UI inspired by
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline pl-1 font-medium text-primary underline underline-offset-4"
+              href="https://svgl.app/"
+            >
+              svgl
+            </Link>
+            ,
+          </div>
+          <div>
+            Built by
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline pl-1 font-medium text-primary underline underline-offset-4"
+              href="https://ro.yikzero.com/"
+            >
+              yikZero
+            </Link>
+            .
+          </div>
         </div>
         <div>
           The source code is available on
